3f7f7b4083e123143ec9637a1c928610a7fffab4
nashid <noor.nashid1@gmail.com>
2022-04-24 10:54:12 -0700

    start with code2graph dataset integration

---
+++ plur/stage_1/conde2graph_dataset.py
@@ -0,0 +1,104 @@
+# Copyright 2021 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Classes for converting the Hoppity single AST diff dataset to a PLUR dataset."""
+import glob
+import json
+import os
+import tarfile
+
+from absl import logging
+import apache_beam as beam
+from plur.stage_1.plur_dataset import Configuration
+from plur.stage_1.plur_dataset import PlurDataset
+from plur.utils import constants
+from plur.utils import util
+from plur.utils.graph_to_output_example import GraphToOutputExample
+from plur.utils.graph_to_output_example import GraphToOutputExampleNotValidError
+import tqdm
+
+
+class Code2GraphDataset(PlurDataset):
+  def __init__(self,
+               stage_1_dir,
+               configuration: Configuration = Configuration(),
+               transformation_funcs=(),
+               filter_funcs=(),
+               user_defined_split_range=(),
+               num_shards=1000,
+               seed=0,
+               deduplicate=False):
+    self.cooked_one_diff_extracted_dir = None
+    self.hoppity_cg_extracted_dir = None
+    super().__init__(
+        self._DATASET_NAME,
+        self._URLS,
+        self._GIT_URL,
+        self._DATASET_DESCRIPTION,
+        stage_1_dir,
+        transformation_funcs=transformation_funcs,
+        filter_funcs=filter_funcs,
+        user_defined_split_range=user_defined_split_range,
+        num_shards=num_shards,
+        seed=seed,
+        configuration=configuration,
+        deduplicate=deduplicate)
+
+  def download_dataset(self):
+    """Download and extract the tar.gz files."""
+    super().download_dataset_using_requests()
+
+  def get_all_raw_data_paths(self):
+    """Get paths to all raw data."""
+    return None
+
+  def raw_data_paths_to_raw_data_do_fn(self):
+    """Returns a beam.DoFn subclass that reads the raw data."""
+    return None
+
+  def raw_data_to_graph_to_output_example(self, raw_data):
+    """Convert raw data to the unified GraphToOutputExample data structure.
+
+    The hoppity single AST diff task is already represented as a graph.
+    We used their input graph structure. For the output, we have 1 to 1 mapping
+    to our output format. We try to mirror the implementation details from
+    the hoppity data processing stage.
+
+    Args:
+      raw_data: A hoppity single AST diff task from the dataset. It is a
+        dictionary with keys 'split', 'buggy_graph' and 'edit_operation'. Values
+        are the split(train/valid/test) the data belongs, the buggy_graph read
+        from the json file and 'edit_operation' read from the text file.
+
+    Raises:
+      GraphToOutputExampleNotValidError if the GraphToOutputExample is not
+      valid.
+
+    Returns:
+      A dictionary with keys 'split' and 'GraphToOutputExample'. Values are the
+      split(train/validation/test) the data belongs, and the
+      GraphToOutputExample instance.
+    """
+    split = raw_data['split']
+
+    graph_to_output_example = GraphToOutputExample()
+
+    # add nodes
+    graph_to_output_example.add_node(node[0], node[1], node[2])
+
+    # add edges
+    graph_to_output_example.add_edge(edge[0], edge[1], str(edge[2]))
+
+    # output
+    graph_to_output_example.add_token_output("return sum;")
+    return {'split': split, 'GraphToOutputExample': graph_to_output_example}
